3c8523067de46c2b507c84b147fdfd9d6e9518c1
Alen Bastías <95000215+BOR34LIS@users.noreply.github.com>
2025-11-12 02:15:08 -0300

    Se arregla el estilo del login ya que antes no estaba centrado

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,16 @@
 import LoginForm from "@/components/login/LoginForm";
 import { Suspense } from "react";
-
+import './loginpage.css';
 function Loading() {
   return <div>Cargando...</div>;
 }
 
 export default function LoginPage() {
   return (
-    <Suspense fallback={<Loading />}>
-      <LoginForm />
-    </Suspense>
+    <main className="login-page-container">
+      <Suspense fallback={<Loading />}>
+        <LoginForm />
+      </Suspense>
+    </main>
   );
 }
